528f65d716114a85da67ba9a93199b9c8063c2b1
emma-martin <emmamartinalvarez@gmail.com>
2018-11-17 16:27:47 +0100

    initial commit

---
+++ python_intro.py
@@ -0,0 +1,34 @@
+# print('Hello, Django girls!')
+# if 5 > 2:
+#     print('5 is indeed greather than 2')
+# else:
+#     print('5 is not greather than 2')
+# if 3 >2:
+#     print('It works!')
+
+# name = 'Sonja'
+
+
+# def hi():
+#     print('Hi there')
+#     print('wacha doin')
+
+# def hi(name):
+#     if name == 'Ola': 
+#         print('Hey Ola')
+#     elif name == 'Sonja':
+#         print ('Hey Sonja')
+#     else:
+#         print ('Hey anonymous')
+# hi('tururur')
+
+def hi(name):
+    print('Hi '+ name + '!')
+
+hi("Rachel")
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
